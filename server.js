a19253ee094abd0aed21a00427a1bd091fab57a0
AriannaFedeli <arianna.fedeli@studenti.unicam.it>
2017-10-11 11:45:06 +0200

    modificato file server

--- server.js
+++ server.js
@@ -1,34 +1,43 @@
 require('dotenv').config();
-    // import moduli node ========================
+
+//import module 
 var express  = require('express');
 var app      = express(); 
 var bodyParser = require('body-parser');            // pull information from HTML POST (express4)
 
 
-// configuration =================
-
+//connect to the database mongoose
 
 var mongoose = require('mongoose');
  mongoose.connect('mongodb://localhost/ProgrammazioneWeb');
-var con = mongoose.connection;
-con.on('error', function (err){
-    console.log('errore di connessione', err);
+var connect = mongoose.connection;
+
+
+connect.once('open', function (){
+    //message on the terminal, connection is ON
+console.log('*    Server connection is Up!      *');
+console.log('************************************')
 });
-
-con.once('open', function (){
-console.log('connessione al database TEST riuscita!');
+connect.on('error', function (err){
+    //if there is an error while connection
+    console.log('++++    errore di connessione   +++++', err);
 });
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({'extended':'true'}));    
+// parse application/json        
+app.use(bodyParser.json());            
+// parse application/vnd.api+json as json                         
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+
+app.use(express.static(__dirname + '/client')); 
 
 
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-
-// routes ======================================================================
+// routes for server file 
 app.use(require('./server/routes.js'));
 
-// listen (start app with node server.js) ======================================
+// listen (start app with node server.js) 
     app.listen(8080,()=>{
-        console.log('server on su http://localhost:' + 8080);
+        console.log('************************************')
+        console.log('*Server on at http://localhost:' + 8080+'*');
     })
 
--- server/routes.js
+++ server/routes.js
@@ -7,13 +7,25 @@ ProfController=require('./function/professori');
 module.exports = router;
 
 // ROUTE 
-    //ROUTER FOR USER
-
-
+    //ROUTER FOR USER = student
 router.post('/addStudent',userController.addStudent); //ok
+router.post('/loginStudent', userController.loginStudent);
+router.post('/addFacolta',userController.addFacolta); //??
+router.post('/addCorso',userController.addCorso); //?? 
+
+    //route for admin
+    //router.post('/removeUser', ProfController.);
+    
+
+
+
+    //ROUTE FOR PROF
+
 router.post('/addProf',ProfController.addProf);
 router.post('/loginProf', ProfController.loginProf);
-router.post('/loginStudent', userController.loginStudent);
 router.post('/addAppello', ProfController.addAppello);
-router.post('/addFacolta',userController.addFacolta);
-router.post('/addCorso',userController.addCorso);
+//router.post('/addCorso', ProfController.);
+//router.post('/closeAppello', ProfController.);
+//router.post('/modifyAppello', ProfController.);
+//router.post('/removeAppello', ProfController.);
+//
