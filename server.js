bc04d997c929aed4379ad5d47495f168cc02a60f
MarkRoss1095 <marcellorosati1095@gmail.com>
2018-01-02 15:08:57 +0100

    ciaoo funziona

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express  = require('express');
 var app = express(); 
 var bodyParser = require('body-parser');
 
-var port = process.env.PORT || 8080;
+/* var port = process.env.PORT || 8080; */
 app.use(express.static(__dirname + '/client'));     
 
 
@@ -36,6 +36,10 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(require('./server/routes.js'));
 
 // listen (start app with node server.js) 
-    app.listen(port,function(){
+/*     app.listen(port,function(){
         console.log('Our app is running on http://localhost:' + port);    })
 
+ */
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
